ac0d7167037ba1287f914de7a9a52c0746359b7c
joy akpan <114065663+joydan1@users.noreply.github.com>
2025-09-28 22:38:26 +0100

    Update supabaseClient.js

--- frontend/supabaseClient.js
+++ frontend/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Replace with your actual Supabase project URL and anon key
 const supabaseUrl='https://jlclgonwsvnlpyholvd.supabase.co';
-const supabaseAnonKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsY2xjZ29ud3N2bmxweWhvbHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NTY2MjQsImV4cCI6MjA3NDIzMjYyNH0.JHx0Q7HcOjS4lsw2GXwU_WHrICQ0it-bhd0Z64ZoBPY
+const supabaseAnonKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsY2xjZ29ud3N2bmxweWhvbHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NTY2MjQsImV4cCI6MjA3NDIzMjYyNH0.JHx0Q7HcOjS4lsw2GXwU_WHrICQ0it-bhd0Z64ZoBPY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
