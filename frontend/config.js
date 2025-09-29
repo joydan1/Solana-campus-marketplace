9500abbc674b3ce42c1d8d4d70c3fb18cd1492ec
joy akpan <114065663+joydan1@users.noreply.github.com>
2025-09-30 00:45:21 +0100

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,6 @@
+Config.js from frontend
 import idl from "./idl.json";
 
 export const IDL = idl;
-export const PROGRAM_ID = "Dh9qpAVZunvQrHuBiMRExS6b8ieCBMdnM3vnRa9SfLJZ";
-export const NETWORK = "https://api.devnet.solana.com";
+export const PROGRAM_ID = "Dh9qpAVZunvQrHuBiMRExS6b8ieCBMdnM3vnRa9SfLJZ"; // update if redeployed
+
